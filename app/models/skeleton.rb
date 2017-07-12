e4f52c2a454c3d2c38361739ada3a64b544ce3c7
Nicole Santamaria and Sara Schultz <sara@>
2017-07-12 16:15:23 -0700

    config paperclip in development.rb

--- app/models/skeleton.rb
+++ app/models/skeleton.rb
@@ -1,4 +1,5 @@
 class Skeleton < ActiveRecord::Base
   belongs_to :closet
   validates(:name, :body, presence: true)
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
 end
--- spec/models/skeleton_spec.rb
+++ spec/models/skeleton_spec.rb
@@ -4,4 +4,5 @@ describe Skeleton do
   it { should belong_to :closet }
   it { should validate_presence_of :name }
   it { should validate_presence_of :body }
+  it { should have_attached_file(:picture) }
 end
