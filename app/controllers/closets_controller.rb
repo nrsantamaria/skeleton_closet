e1da7d2d196bd893782a54c13449d5d2640dc1e0
Nicky Santamaria and Sara Schultz <nicky@>
2017-07-13 14:00:32 -0700

    add funcitonality to hide skeleton in closet

--- app/controllers/closets_controller.rb
+++ app/controllers/closets_controller.rb
@@ -8,4 +8,14 @@ class ClosetsController < ApplicationController
     @visible_skeletons = @closet.skeletons.where(hide: false)
     @hidden_skeletons = @closet.skeletons.where(hide: true)
   end
+
+  # def update
+  #   @closet = Closet.find(params[:closet_id])
+  #   @skeleton = Skeleton.find(params[:id])
+  #   if @closet.update(skeleton_params)
+  #     redirect_to closet_path(@skeleton.closet)
+  #   else
+  #     render :edit
+  #   end
+  # end
 end
